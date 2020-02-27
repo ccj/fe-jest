d4f80c11288d6854fb77099cd4103251a19355ab
ccj <981015619@qq.com>
2020-02-27 20:06:19 +0800

    匹配器

--- lesson4/matchers.test.js
+++ lesson4/matchers.test.js
@@ -1,3 +1,10 @@
-test('测试加法3+7',() => {
-    expect(10). toBe(10);
+test('测试10与10相匹配',() => {
+    // toBe 匹配器 matchers 类似于===
+    expect(10).toBe(10);
+})
+
+test('测试对象内容相等',() => {
+    // toEqual 匹配器
+    const a = { one: 1 };
+    expect(a).toEqual({ one: 1 });
 })
